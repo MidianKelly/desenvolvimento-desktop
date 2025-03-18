b2b1885f233e3d607282d945a2f7c7daad2b5120
Midian Kelly <senac.midiankellysantos@gmail.com>
2025-03-17 21:57:23 -0300

    Feat: Adicionei o formulário carteirinha. finalizando a  parte de infantil da carteirinha #9

---
+++ MultApps/MultApp.Windows/CarteirinhaImagens.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultApp.Windows
+{
+    internal class CarteirinhaImagens
+    {
+        public const string Criança = "https://static.vecteezy.com/system/resources/thumbnails/009/340/195/small_2x/child-expression-flat-design-illustration-cute-kid-cartoon-for-avatar-profile-png.png";
+        public const string Jovens = "https://thumbs.dreamstime.com/b/estilo-de-destaque-do-%C3%ADcone-jovens-felizes-vetor-contorno-para-design-da-web-isolado-em-fundo-branco-196669889.jpg";
+        public const string Adultos = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fbr%2Fvector%2Fadult-aunt-auntie-icon-solid-style-459778430.html&psig=AOvVaw0_9eTUgr0lcFepkHdS5FSP&ust=1742342754600000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDZ0dmqkowDFQAAAAAdAAAAABAE";
+        public const string Idosos = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.vecteezy.com%2Farte-vetorial%2F17119037-icone-de-linha-para-idosos&psig=AOvVaw06swqz5Z9PdVCt9X330prL&ust=1742342780564000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKiO7eWqkowDFQAAAAAdAAAAABAE";
+    }
+}
--- MultApps/MultApp.Windows/frmCarteirinha.cs
+++ MultApps/MultApp.Windows/frmCarteirinha.cs
@@ -16,5 +16,42 @@ namespace MultApp.Windows
         {
             InitializeComponent();
         }
+
+        private void btnGerarCarteirinha_Click(object sender, EventArgs e)
+        {
+            #region Inserir nome na carteirinha
+
+            var nome = txtNome.Text;
+            lblNomeResultado.Text = $@"Nome:{nome}"; ;
+
+            #endregion
+
+            #region Definir idade
+            var nascimento = DateTime.Parse(dateTimePicker1.Text);
+            var hoje = DateTime.Now.Year;
+
+            var idade = hoje - nascimento.Year;
+
+            lblIdade.Text = $@"Idade:{idade} anos";
+            
+
+            if (idade < 12)
+            {
+              
+               pictureBox1.Load (CarteirinhaImagens.Criança);
+               lblInformacao.Text = $@"CRIANÇA";
+               lblInformacao.Visible = true;
+               panel1.BackColor = Color.Azure;
+
+
+            }
+
+
+
+
+            #endregion
+        }
+
+       
     }
 }
