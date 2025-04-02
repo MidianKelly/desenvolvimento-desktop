45598aa484797ee82dbf18b1ffa8fff15fedbe7b
Midian Kelly <senac.midiankellysantos@gmail.com>
2025-04-01 21:38:10 -0300

    feat: adiciona script de banco de dados para as tabelas de produto e categoria.

---
+++ MultApps/MODEL/MultiApps.Models/Entities/Produto.cs
@@ -0,0 +1,18 @@
+﻿using MultiApps.Models.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiApps.Models.Entities
+{
+    internal class Produto : EntidadeBase
+    {
+        public int CategoriaId { get; set; }
+        public string nome { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+        
+    }
+}
